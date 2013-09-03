f1235874e05d33ab2557a77478b4291e3d9fb67d
calebx <caleb.xiang@gmail.com>
2013-09-03 17:37:41 +0800

    fix 0.003 bugs

--- lib/number_to_cn/methods.rb
+++ lib/number_to_cn/methods.rb
@@ -1,59 +1,59 @@
 #encoding:utf-8
 module NumberToCn 
-  CN_T_TRANS = [ "", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾" ]
+  CN_T_TRANS           = [ "", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾" ]
   CN_T_TRANS_WITH_ZERO = [ "零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾" ]
-  CN_T_POSITION = [ "", "拾", "佰", "仟" ]
-  CN_T_BIG = [ "", "萬", "亿", "萬"]
+  CN_T_POSITION        = [ "", "拾", "佰", "仟" ]
+  CN_T_BIG             = [ "", "萬", "亿", "萬" ]
 
   def to_cn_words
     if self.class == Fixnum
-      return "零" if self == 0
-      num_arr = self.to_s.split("").reverse
-      rst_arr = []
+      int_words
+    elsif self.class == Float
+      float_words
+    end
+  end
 
-      writable = -1 
-      #1：0是需要写的。
-      #0：0不需要写； 
-      #-1：全不需要写；
+  def int_words
+    return "零" if self == 0
 
-      num_arr.each_with_index do |value, index|
-        writable = -1 if index%4 == 0
+    num_arr = self.to_s.split('').reverse
+    rst_arr = []
 
-        cc = (index % 4 == 0) ? CN_T_BIG[index/4] : ""
-        aa = CN_T_TRANS[value.to_i]
+    writable = -1 
+    #1： 都需要写；
+    #0： 写一个零，后面的'千'不用写； 
+    #-1：不需要写'千'，也不需要写'零'；
 
-        if value.to_i == 0
-          writable = (writable == 1) ? 0 : -1
-        else
-          writable = 1
-        end
+    num_arr.each_with_index do |value, index|
+      writable = -1 if index%4 == 0
+      cc = (index%4 == 0) ? CN_T_BIG[index/4] : ""
+      aa = CN_T_TRANS[value.to_i]
 
-        if writable == 1
-          bb = CN_T_POSITION[index%4]
-        elsif writable == -1
-          bb = ""
-        else
-          bb = "零"
-        end
-
-        rst_arr << aa + bb + cc
+      if value.to_i == 0
+        bb = (writable == 1) ? "零" : ""
+        writable = (writable == 1) ? 0 : -1
+      else
+        bb = CN_T_POSITION[index%4]
+        writable = 1
       end
 
-      rst_arr.reverse.join
-    elsif self.class == Float
-      before_point = self.to_s.split(".")[0]
-      after_point = self.to_s.split(".")[1]
-      "#{before_point.to_i.to_cn_words}点#{after_point.to_i.to_cn_clearly}"
+      rst_arr << aa + bb + cc
     end
+
+    rst_arr.reverse.join
+  end
+
+  def float_words
+    before_point     = self.to_i
+    after_point      = self.to_s.split(".")[1]
+    after_point_zero = "零" * (after_point.length - after_point.to_i.to_s.length)
+    "#{before_point.int_words}点#{after_point_zero}#{after_point.to_i.to_cn_clearly}"
   end
   
   def to_cn_clearly
-    digits_arr = self.to_s.split('')
-    rst = ""
-    digits_arr.map do |d|
-      rst << CN_T_TRANS_WITH_ZERO[d.to_i]
-    end
-    rst
+    to_s.split('')
+        .delete_if{ |c| c =~ /\D/ }
+        .map{ |c| CN_T_TRANS_WITH_ZERO[c.to_i] }
+        .join
   end
-
 end
--- spec/number_to_cn_spec.rb
+++ spec/number_to_cn_spec.rb
@@ -6,8 +6,32 @@ describe NumberToCn do
     (2203.to_cn_words).should eq("贰仟贰佰零叁")
   end
 
+  it "should go works for 2003" do
+    (2003.to_cn_words).should eq("贰仟零叁")
+  end
+
+  it "should go works for 20003" do
+    (20003.to_cn_words).should eq("贰萬零叁")
+  end
+
+  it "should go works for 200003" do
+    (200003.to_cn_words).should eq("贰拾萬零叁")
+  end
+
   it "worked for zero" do
     (0.to_cn_words).should eq("零")
+  end
+
+  it "worked for 0.0" do
+    (0.0.to_cn_words).should eq("零点零")
+  end
+
+  it "worked for 0.003" do
+    (0.003.to_cn_words).should eq("零点零零叁")
+  end
+
+  it "worked for 0.0" do
+    (0.02.to_cn_words).should eq("零点零贰")
   end
 
   it "worked for float" do
